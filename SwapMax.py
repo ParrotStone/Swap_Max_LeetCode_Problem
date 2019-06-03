c6e41522c3ffafc6accec1479fdee6fe12b35612
Abdulrahmn Ali Hussain <abdulrahmanalihussainM@gmail.com>
2019-06-03 15:24:20 +0200

    Modified_Code_Solving_Some_Bugs

--- SwapMax.py
+++ SwapMax.py
@@ -2,11 +2,19 @@
 
 # # First version
 
+
 class Solution:
     def maximum_swap(self, num: int) -> int:
         digit_list = list(str(num))
-        mx_index = digit_list.index(max(digit_list))
-        digit_list[0], digit_list[mx_index] = digit_list[mx_index], digit_list[0]
+        swapped = False
+        i = 0
+        while not swapped and i < len(digit_list):
+            mx_index = digit_list.index(max(digit_list[i:]), i)
+            if digit_list[i] < digit_list[mx_index]:
+                digit_list[i], digit_list[mx_index] = digit_list[mx_index], digit_list[i]
+                swapped = True
+            else:
+                i += 1
 
         result = ''
         for i in digit_list:
@@ -17,12 +25,14 @@ class Solution:
 
 solution = Solution()
 print(solution.maximum_swap(234))  # 432
-print(solution.maximum_swap(2736))  # 7632
+print(solution.maximum_swap(2736))  # 7236
 print(solution.maximum_swap(9973))  # 9973
-print(solution.maximum_swap(35995))  # 99553
+print(solution.maximum_swap(9783))  # 9873
+print(solution.maximum_swap(98368))  # 98863
+print(solution.maximum_swap(1993))  # 9913
+
 
 # # Second Version - Best one
-
 # class Solution:
 #     def maximum_swap(self, num: int) -> int:
 #         digit_list = list(str(num))
@@ -43,7 +53,6 @@ print(solution.maximum_swap(35995))  # 99553
 
 
 # # Third version _ using sort method in Python
-
 # class Solution:
 #     def maximum_swap(self, num: int) -> int:
 #         digit_list = list(str(num))
@@ -53,7 +62,6 @@ print(solution.maximum_swap(35995))  # 99553
 #         	result += i
 
 #         return int(result)
-
 
 
 # solution = Solution()
